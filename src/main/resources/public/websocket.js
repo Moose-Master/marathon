760677823009feb137dcb139363ddac3571f01ab
moose-master <korivan888@gmail.com>
2025-07-27 16:17:56 -0500

    moved scroller fucntionality into websocket and removed the scroller page

--- src/main/resources/public/websocket.js
+++ src/main/resources/public/websocket.js
@@ -1,3 +1,4 @@
+let uname = "PlaceholderPerson"
 let socket = null;
 let input_box = null;
 
@@ -5,13 +6,23 @@ function start() {
     console.log("Hello, world!");
     input_box = document.getElementById("msg");
 
+    let scrollerContent = document.getElementById('scrollerContent');
+
+    let c = document.cookie
+
+    if(c.substring(0,c.indexOf('=')-1) == "username"){
+    uname = c.substring(c.indexOf('=')+1)
+    }
+    console.log(`User is: ${uname}`)
+
+
     const PATH = `/ws`;
     console.log(PATH);
     socket = new WebSocket(PATH);
     socket.addEventListener("message", (event) => {
         let msg = event.data;
         console.log(`Message: ${msg}`);
-        document.getElementById("wsout").innerText += msg + "\n";
+        scroll(msg)
     });
     socket.addEventListener("close", (event) => {
         console.log("Disconnected");
@@ -19,18 +30,28 @@ function start() {
     socket.addEventListener("error", (event) => {
         console.log(`Error: ${event}`);
     });
-    input_box.addEventListener("keypress", (event) => {
-        if (event.key == "Enter") {
-            sendMessage();
-        }
-    });
 }
+
+function sendEnter(e) {
+   if((e && e.keyCode == 13) || e == 0) {
+     let value = input_box.value;
+     document.forms.form01.reset();
+     sendMessage();
+       
+   }
+   
+   function scroll(mesg) {
+  let newChild = scrollerContent.lastElementChild.cloneNode(true);
+  newChild.innerHTML = mesg
+  scrollerContent.appendChild(newChild)
+};
+}
+
 function sendMessage() {
     if (socket.readyState == WebSocket.OPEN) {
-        let value = input_box.value;
+        let value = (uname + ": " + input_box.value)
         socket.send(value);
         console.log(`Sent message ${value}`);
-        input_box.value = "";
     } else {
         console.log(socket.readyState);
     }
